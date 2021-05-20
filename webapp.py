76afd942bc6a7cecead4ab1aa1cf908cbdb7aaa3
MinecraftGodkmjngun <77691179+MinecraftGodkmjngun@users.noreply.github.com>
2021-05-20 16:01:25 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -9,7 +9,7 @@ def render_main():
 @app.route("/numberresults")
 def render_page1():
     x=request.args['numberrequest'] #setting x to the first number
-    x
+    x=float(x)
     return render_template('layout.html', number1=x*x, number2=x*x*x, number3=x*x*x*x, number4=x*x*x*x*x, number5=x*x*x*x*x*x, number6=x*x*x*x*x*x*x, number7=x*x*x*x*x*x*x*x, number8=x*x*x*x*x*x*x*x*x, number9=x*x*x*x*x*x*x*x*x*x, number10=x*x*x*x*x*x*x*x*x*x*x) #rendering results
     
 if __name__=="__main__":
